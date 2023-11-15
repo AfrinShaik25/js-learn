6592dda0ee4483d578295a0dca053b453d43680d
AfrinShaik25 <150754920+AfrinShaik25@users.noreply.github.com>
2023-11-15 16:02:01 +0000

    variables learning

---
+++ jsbasics/01-variables.js
@@ -0,0 +1,16 @@
+const accountId=58963
+var accountPassword="852369"
+let accountEmail="variable@gmail.com"
+accountCity="nellore"
+let accountState;
+
+//accountId=2 
+
+accountEmail="hi@google.com"
+accountPassword="785296"
+accountCity="bangaloe"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*prefer  not use var -issue in block scope and functional scope*/
