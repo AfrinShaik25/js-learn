398515fd7d8c0cfe25501a9d4feec1f8008787b3
AfrinShaik25 <150754920+AfrinShaik25@users.noreply.github.com>
2023-11-17 01:17:45 +0000

    datatypes

---
+++ jsbasics/02-datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer js version
+//alert(3+3) //gets error cause we are using node js not browser
+//code readability should be high
+
+
+let name="afrin"
+let age=25
+let isLoggedIn=true
+let state;
+
+//number => 2 to power 53
+//string = >""
+//boolean = true/falsa
+//bigint
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
